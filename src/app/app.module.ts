732e13c1bfa2aaa9b367b00f14a90ca09daf1e47
tavodevcode <tavodevcode@gmail.com>
2024-01-14 10:49:15 -0600

    Add Tailwind CSS and Google Fonts, update Angular imports, and add routing module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,21 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { NgModule, OnInit } from '@angular/core'
+import { BrowserModule } from '@angular/platform-browser'
 
-import { AppComponent } from './app.component';
+import { initFlowbite } from 'flowbite'
+
+import { AppComponent } from './app.component'
+import { SharedModule } from './shared/shared.module'
+import { AppRoutingModule } from './app-routing.module'
+import { HttpClientModule } from '@angular/common/http'
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule, SharedModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule implements OnInit {
+  ngOnInit(): void {
+    initFlowbite()
+  }
+}
