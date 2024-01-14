0d7aaba1493953e651145769d8462063e9a04758
tavodevcode <tavodevcode@gmail.com>
2024-01-14 10:48:47 -0600

    Add country page component HTML and TypeScript files

---
+++ src/app/countries/pages/country-page/country-page.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit } from '@angular/core'
+import { ActivatedRoute, Router } from '@angular/router'
+import { CountriesService } from '../../services/countries.service'
+import { switchMap, count } from 'rxjs'
+import { Country } from '../../interfaces/country.interface'
+
+@Component({
+  selector: 'app-country-page',
+  templateUrl: './country-page.component.html',
+  styles: []
+})
+export class CountryPageComponent implements OnInit {
+  public country?: Country
+
+  constructor(
+    private activaredRoute: ActivatedRoute,
+    private countriesService: CountriesService,
+    private router: Router
+  ) {}
+
+  ngOnInit(): void {
+    this.activaredRoute.params
+      .pipe(switchMap(({ id }) => this.countriesService.searchCountryByCode(id)))
+      .subscribe((country) => {
+        if (!country) return this.router.navigateByUrl('')
+
+        return (this.country = country)
+      })
+  }
+
+  // public searchCountryByCode = (params: Params): void => {
+  //   this.countriesService.searchCountryByCode(params['id']).subscribe((country) => {
+  //     console.log({ country })
+  //   })
+  // }
+}
